ee2ace8c9551abdf26496855a4588425d845bbf7
7BIT-CODED <168307922+7BIT-CODED@users.noreply.github.com>
2024-04-27 14:44:57 -0700

    Create Battleship.py

---
+++ Battleship.py
@@ -0,0 +1,14 @@
+import random
+
+rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+orientaton =["dow", "acc"]
+
+class Ship:
+	def __init__ (self, user, length, orientation):
+		self.user = user
+		self.legnth = length 
+		self.orientation = orientation 
+
+
+print(rows[3])
